d73976c0dbadfc5c25c92969f21d2059c0b20486
tetsuwo <tetsuwo.oishi@gmail.com>
2016-12-24 06:58:56 +0900

    Remove scripts/test.js

--- scripts/test.js
+++ scripts/test.js
@@ -13,20 +13,20 @@
 var CronJob = require('cron').CronJob;
 
 module.exports = function(robot) {
-  var postRoom = process.env.HUBOT_SLACK_TEST_POST_ROOM;
-
-  var postMessage = '';
-  postMessage += '@here\n';
-  postMessage += 'みなさん、テスト投稿ですよ。';
-
-  new CronJob(
-    '00 10 10 * * 1-5',
-    function() {
-      robot.send({ room: postRoom }, postMessage);
-      robot.logger.info('Post a test message!');
-    },
-    null,
-    true,
-    'Asia/Tokyo'
-  );
+//  var postRoom = process.env.HUBOT_SLACK_TEST_POST_ROOM;
+//
+//  var postMessage = '';
+//  postMessage += '@here\n';
+//  postMessage += 'みなさん、テスト投稿ですよ。';
+//
+//  new CronJob(
+//    '00 10 10 * * 1-5',
+//    function() {
+//      robot.send({ room: postRoom }, postMessage);
+//      robot.logger.info('Post a test message!');
+//    },
+//    null,
+//    true,
+//    'Asia/Tokyo'
+//  );
 };
